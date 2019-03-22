acf9d90b7da4252899f6979f57aacb5721171e11
Katsushi OUGI <bdbkx286@gmail.com>
2019-03-22 14:03:14 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CssBaseline from "@material-ui/core/CssBaseline"
+import React, { Fragment } from "react"
+
+const App: React.FC = () => (
+  <Fragment>
+    <CssBaseline />
+    <h1>Hello React</h1>
+  </Fragment>
+)
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,91 @@
+const path = require("path")
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin")
+const CleanWebpackPlugin = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+const ENV = process.env.NODE_ENV || "development"
+const isProd = ENV !== "development"
+const output = "public"
+const firebaseConfig = require("firebase-tools/lib/config").load({
+  cwd: process.cwd()
+})
+
+module.exports = {
+  mode: ENV,
+  entry: ["./src/index.tsx"],
+  output: {
+    filename: "[name].[hash].js",
+    chunkFilename: "[id].[hash].js",
+    publicPath: "/",
+    path: path.resolve(__dirname, output)
+  },
+  optimization: {
+    splitChunks: {
+      name: "vendor",
+      chunks: "initial"
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html"
+    }),
+    // 型エラーのみを検知する
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.*(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: [].concat(
+          isProd
+            ? [
+                {
+                  loader: "babel-loader"
+                },
+                {
+                  loader: "ts-loader",
+                  options: {
+                    // 型を無視する
+                    happyPackMode: true
+                  }
+                }
+              ]
+            : [
+                {
+                  // webpackをマルチスレッド化
+                  loader: "thread-loader",
+                  options: {
+                    workers: require("os").cpus().length - 1,
+                    poolTimeout: Infinity
+                  }
+                },
+                {
+                  loader: "babel-loader"
+                },
+                {
+                  loader: "ts-loader",
+                  options: {
+                    // 型を無視する
+                    happyPackMode: true
+                  }
+                }
+              ]
+        )
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    contentBase: "public",
+    open: true,
+    before(app) {
+      app.use(require("superstatic")({ config: firebaseConfig.data.hosting }))
+    }
+  }
+}
